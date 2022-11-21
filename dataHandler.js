6d71ddddc49e083243de0527872307cd212739a5
code-a-man <43219246+code-a-man@users.noreply.github.com>
2022-11-21 13:57:55 +0300

    move updateSheet func to dataHandler from index.js

--- dataHandler.js
+++ dataHandler.js
@@ -1,6 +1,11 @@
 import { GoogleSpreadsheet } from 'npm:google-spreadsheet';
+import { config as loadEnv } from "https://deno.land/std@0.165.0/dotenv/mod.ts";
 
 const updateSheet = async () => {
+
+	await loadEnv({ export: true, allowEmptyValues: true });
+
+	console.log('Updating sheet');
 	const doc = new GoogleSpreadsheet(Deno.env.get("SHEET_ID"));
 	doc.useApiKey(Deno.env.get("API_KEY"));
 
@@ -9,7 +14,14 @@ const updateSheet = async () => {
 
 	const rows = await sheet.getRows();
 	const lastUpdate = new Date();
+	console.log("Updated sheet");
 	return {rows, lastUpdate};
 };
 
-export { updateSheet };
+let data = await updateSheet();
+
+setInterval(async () => {
+	data = await updateSheet();
+}, 300000);
+
+export { updateSheet, data };
--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { router } from "./routes.js";
-import { updateSheet } from "./dataHandler.js";
 
-let data = await updateSheet();
 const app = new Application();
 
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-setInterval(async () => {
-	  data = await updateSheet();
-}, 300000);
 
 await app.listen({ port: 8000 });
