acc6a9a4df869a445f402b9a2189d97187a5dee4
code-a-man <43219246+code-a-man@users.noreply.github.com>
2022-11-21 13:58:28 +0300

    create example /data route for tests

--- routes.js
+++ routes.js
@@ -1,10 +1,16 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+import { data } from "./dataHandler.js";
 
 const router = new Router();
 
 router.get("/", ({response}) => {
   response.body = { message: "Hello World!" };
   response.status = 200;
+});
+
+router.get("/data", ({response}) => {
+	  response.body = { message: data.lastUpdate }
+	  response.status = 200;
 });
 
 export { router };
